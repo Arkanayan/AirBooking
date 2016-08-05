a0fcd3ee96d6c791ceb9c8855f19a2cfb0190e24
Arka Nayan <itsarkanayan@gmail.com>
2016-08-05 10:58:17 +0530

    Update bootstrap version, add ticket booking modal

--- index.php
+++ index.php
@@ -13,16 +13,46 @@
     <script src="js/jquery-3.1.0.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
       <script src="js/bootstrap-notify.min.js" type="text/javascript"></script>
+
+      <link href="css/animate.css" rel="stylesheet">
   </head>
   <body>
   <header class="navbar navbar-fixed-top navbar-brand"><h3>Air Booking</h3></header>
   <header class="page-header"></header>
     <div class="container">
 
-      <div class="form-signin">
-        <a href="login/logout.php" class="btn btn-default btn-lg btn-block">Logout</a>
-      </div>
+        <div class="row">
+            <button type="button" class="btn btn-info btn-lg center-block"
+                    data-toggle="modal" data-target="#bookTicketModal">
+                Book a ticket</button>
+
+            <div class="form-signin">
+
+                <a href="login/logout.php" class="btn btn-default btn-lg btn-block">Logout</a>
+            </div>
+        </div>
+
+
     </div> <!-- /container -->
+
+  <!-- Modal -->
+  <div class="modal fade " id="bookTicketModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+      <div class="modal-dialog modal-lg" role="document">
+          <div class="modal-content">
+              <div class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                  <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+              </div>
+              <div class="modal-body">
+                  ...
+              </div>
+              <div class="modal-footer">
+                  <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-primary">Save changes</button>
+              </div>
+          </div>
+      </div>
+  </div>
 
   <script>
       $(document).ready(function () {
@@ -33,11 +63,11 @@
           },{
               // settings
               type: 'success',
-              timer: 1000,
               offset: 20,
               spacing: 10,
               z_index: 1031,
-              delay: 5000,
+              delay: 2000,
+              allow_dismiss: true,
               animate: {
                   enter: 'animated fadeInDown',
                   exit: 'animated fadeOutUp'
